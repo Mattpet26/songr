d3bf71c359296a44bea33ebe091ff1f813599290
Matthew <Mattpet26@gmail.com>
2020-10-07 10:05:14 -0700

    added /hello route

--- src/main/java/com/petersen/songr/AlbumController.java
+++ src/main/java/com/petersen/songr/AlbumController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.ArrayList;
@@ -52,4 +51,9 @@ public class AlbumController {
         mPotato.addAttribute("album", album);
         return "singlealbum";
     }
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
 }
